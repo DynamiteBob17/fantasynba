41e18dab971584c3ef8c783436980345fcac9fa5
DynamiteBob17 <jan.mlinaric@gmail.com>
2024-12-19 17:52:56 +0100

    feat: improve player parsing, implement FileHandler

--- src/main/java/io/parser/PlayerParser.java
+++ src/main/java/io/parser/PlayerParser.java
@@ -1,18 +1,87 @@
 package io.parser;
 
 import data.Player;
+import io.file.FileHandler;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.util.List;
+
 public class PlayerParser {
 
-    private PlayerParser() {}
+    private PlayerParser() {
+    }
+
+    public static JSONObject getPlayersJsonFromFile(
+            String filePath,
+            String[] keys
+    ) {
+        try {
+            List<String> lines = FileHandler.readLines(filePath);
+            JSONObject json = new JSONObject();
+            JSONArray players = new JSONArray();
+            JSONObject player = new JSONObject();
+
+            int nonNumericalKeysAmount = 2;
+            String[] numericalKeys = new String[keys.length - nonNumericalKeysAmount];
+            System.arraycopy(keys, nonNumericalKeysAmount, numericalKeys, 0, numericalKeys.length);
+
+            int dataIndex = 0;
+            for (String line : lines) {
+                line = line.trim();
+
+                if (line.isBlank() ||
+                        line.matches("[i!]") ||
+                        line.startsWith("Player")) {
+                    continue;
+                }
+
+                if (dataIndex > 2) {
+                    players.put(player);
+                    player.clear();
+                    dataIndex = 0;
+                }
+
+                switch (dataIndex) {
+                    case 0 -> player.put(keys[0], line);
+                    case 1 -> player.put(keys[1], line);
+                    case 2 -> {
+                        String[] parts = line.split("[ \t]");
+
+                        player.put(numericalKeys[0], Double.parseDouble(parts[0]));
+                        // ignore the 4th column as we don't care about Sel. %
+                        try {
+                            for (int i = 2; i < parts.length; ++i) {
+                                player.put(numericalKeys[i - 1], Double.parseDouble(parts[i]));
+                            }
+                        } catch (Exception e) {
+                            System.err.println(e.getLocalizedMessage());
+                            return null;
+                        }
+                    }
+                    default -> {
+                        System.err.println("Something went wrong.");
+                        return null;
+                    }
+                }
+
+                ++dataIndex;
+            }
+
+            json.put("players", players);
+            return json;
+        } catch (IOException e) {
+            System.err.println(e.getLocalizedMessage());
+            return null;
+        }
+    }
 
     public static Player[] getPlayersFromJson(
             JSONObject json,
             boolean withAdditional,
-            String[] playerValueKeys
+            String[] keys
     ) {
         try {
             JSONArray playersJson = json.getJSONArray("players");
@@ -22,23 +91,23 @@ public class PlayerParser {
                 JSONObject playerJson = playersJson.getJSONObject(i);
                 Player.Builder builder = new Player.Builder();
 
-                builder.name(playerJson.getString(playerValueKeys[0]))
-                        .team(playerJson.getString(playerValueKeys[1]))
-                        .salary(playerJson.getDouble(playerValueKeys[2]))
-                        .form(playerJson.getDouble(playerValueKeys[3]))
-                        .tp(playerJson.getDouble(playerValueKeys[4]));
+                builder.name(playerJson.getString(keys[0]))
+                        .team(playerJson.getString(keys[1]))
+                        .salary(playerJson.getDouble(keys[2]))
+                        .form(playerJson.getDouble(keys[3]))
+                        .tp(playerJson.getDouble(keys[4]));
 
                 if (withAdditional) {
-                    builder.withAdditional(playerValueKeys[5], playerJson.getDouble(playerValueKeys[5]));
+                    builder.withAdditional(keys[5], playerJson.getDouble(keys[5]));
                 }
 
                 players[i] = builder.build();
             }
 
             return players;
-        } catch (JSONException e) {
+        } catch (Exception e) {
             System.err.println(e.getLocalizedMessage());
-            return new Player[0];
+            return null;
         }
     }
 
